1e81565012a818eb565af7b55556fc76474b141b
novakov_alex <novakov.alex@gmail.com>
2016-04-16 22:33:56 +0200

    get orders from database; stop application once all orders are in db.

--- src/main/java/tutorial/App.java
+++ src/main/java/tutorial/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import scala.concurrent.ExecutionContextExecutor;
+import tutorial.dal.OrderDao;
 import tutorial.gateway.OrderGateway;
 
 import java.util.Optional;
@@ -14,6 +15,8 @@ import static akka.dispatch.Futures.future;
 
 @ComponentScan
 public class App {
+  private static final int ORDER_COUNT = 10;
+  private static final int MAX_DELAY = 30_000;
 
   public static void main(String[] args) throws InterruptedException {
     SpringApplication app = new SpringApplication(App.class);
@@ -23,17 +26,33 @@ public class App {
     try {
       context = Optional.ofNullable(app.run(args));
       generateRequests(context.orElseThrow(() -> new RuntimeException("Spring context is unavailable")));
-      Thread.sleep(30_000);
+      waitForPersistence(context.get());
     } finally {
       context.map(c -> c.getBean(ActorSystem.class)).ifPresent(ActorSystem::shutdown);
     }
+  }
+
+  private static void waitForPersistence(ConfigurableApplicationContext context) throws InterruptedException {
+    OrderDao orderDao = context.getBean(OrderDao.class);
+
+    int step = 5_000;
+    int delay = step;
+    while (true) {
+      Thread.sleep(delay);
+
+      if (delay >= MAX_DELAY || orderDao.getOrders().size() == ORDER_COUNT) break;
+      delay += step;
+    }
+
+    System.out.println("Orders in db: ");
+    orderDao.getOrders().forEach(o -> System.out.println(o.toString()));
   }
 
   private static void generateRequests(ConfigurableApplicationContext context) {
     ExecutionContextExecutor executor = context.getBean(ActorSystem.class).dispatcher();
 
     OrderGateway orderGateway = context.getBean(OrderGateway.class);
-    IntStream.range(0, 10).forEach(i ->
+    IntStream.range(0, ORDER_COUNT).forEach(i ->
             future(() -> {
               orderGateway.placeOrder();
               return 1;
--- src/main/java/tutorial/dal/OrderDao.java
+++ src/main/java/tutorial/dal/OrderDao.java
@@ -1,9 +1,11 @@
 package tutorial.dal;
 
-import tutorial.om.Order;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import tutorial.om.Order;
+
+import java.util.List;
 
 @Repository
 public class OrderDao {
@@ -12,5 +14,9 @@ public class OrderDao {
 
   public void saveOrder(Order order) {
     sqlSession.insert("tutorial.dal.OrderDao.saveOrder", order);
+  }
+
+  public List<Order> getOrders() {
+    return sqlSession.selectList("tutorial.dal.OrderDao.getOrders");
   }
 }
